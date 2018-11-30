e3d776c577a5016fa79881f8680530eda43c9c43
Jonathan Dharmawan <jonathan.digindo@gmail.com>
2018-11-30 15:34:45 +0700

    update git ignore

---
+++ src/providers/enviroment/enviroment.example.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+
+/*
+  Generated class for the EnviromentProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class EnviromentProvider {
+
+	public base_url:string = "";
+	public gmap_api:string = "";
+	public tomtom_api:string ="";
+	public client_secret:string = "";
+	public client_id:number = 0;
+
+	constructor(public http: HttpClient) {
+	    // console.log('Hello EnviromentProvider Provider');
+	}
+
+}
